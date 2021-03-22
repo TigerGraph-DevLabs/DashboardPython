870d397b253e68c3b4140bbff0daffb7d1e41c1d
Akash <aksah@Akashs-MacBook-Pro.local>
2021-03-22 08:23:26 -0500

    community dashboard updates

--- app.py
+++ app.py
@@ -1,7 +1,19 @@
 from flask import Flask
 from flask import render_template
 from flask import request, send_from_directory
+from flask.json import jsonify
 import pyTigerGraphBeta as tg 
+
+import requests 
+import json
+
+headers = {
+    'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.182 Safari/537.36',
+    'accept': 'application/json',
+    'accept-encoding': 'gzip, deflate, br',
+    'Api-Key' : '360ff2183c95508261fe9c72469821368d91ddd6f324e5197790a64feab6a1b4',
+    'Api-Username': 'Zrouga'
+}
 
 # configs = {
 #     "server" : "https://rasa.i.tgcloud.io",
@@ -88,7 +100,7 @@ def testChartAnt():
 @app.route('/pie')
 def testPieAnt():
    
-    return render_template('/G6/LargeGraph.html')
+    return render_template('/G2/CalendarHeatmap.html')
 
 @app.route('/intents')
 def intents():
@@ -99,6 +111,57 @@ def intents():
     res = []
     return render_template('table.html',res=res)
 
+@app.route('/communityOverview')
+def communityOverview():
+    stats = requests.get("https://community.tigergraph.com/about.json", headers=headers)
+    if (stats.status_code == 200):
+        stats = stats.json()['about']['stats']
+    return render_template('communityOverview.html', stats=stats)
+
+@app.route('/communityUsers')
+def communityUsers():
+    users = requests.get("https://community.tigergraph.com/admin/users/list/active.json?order=posts_read", headers=headers)
+    if (users.status_code == 200):
+        users = users.json()
+        for user in users:
+            user['avatar_template'] = user['avatar_template'].replace("{size}", "100")
+    return render_template('communityUsers.html', users=users)
+
+@app.route('/community/fetch/', methods=['POST', 'GET'])
+def fetchData():
+    pageviews = []
+    signups = []
+    posts = []
+    topics =[]
+    likes = []
+    if request.method == 'POST':
+        dates = request.json['data']
+        url = f"https://community.tigergraph.com/admin/reports/consolidated_page_views.json?{dates}"
+        res = requests.get(url, headers=headers)
+        if (res.status_code == 200):
+            pageviews = res.json()['report']['data']
+       
+        url = f"https://community.tigergraph.com/admin/reports/signups.json?{dates}"
+        res = requests.get(url, headers=headers)
+        if (res.status_code == 200):
+            signups = res.json()['report']['data']
+        
+        url = f"https://community.tigergraph.com/admin/reports/posts.json?{dates}"
+        res = requests.get(url, headers=headers)
+        if (res.status_code == 200):
+            posts = res.json()['report']['data']
+       
+        url = f"https://community.tigergraph.com/admin/reports/topics.json?{dates}"
+        res = requests.get(url, headers=headers)
+        if (res.status_code == 200):
+            topics = res.json()['report']['data']
+        
+        url = f"https://community.tigergraph.com/admin/reports/likes.json?{dates}"
+        res = requests.get(url, headers=headers)
+        if (res.status_code == 200):
+            likes = res.json()['report']['data']
+    
+    return {"pageviews": pageviews, "signups": signups, "posts": posts, "topics": topics, "likes": likes}
 
 @app.route('/add', methods=['POST'])
 def intents_add():
